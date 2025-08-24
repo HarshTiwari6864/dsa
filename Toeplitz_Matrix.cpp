0d524021a72392c7618aae87e6a6d10f110afe8a
HarshTiwari6864 <harshtiwari6864@gmail.com>
2025-08-25 00:41:59 +0530

    maths

---
+++ Toeplitz_Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                if (matrix[i][j] != matrix[i - 1][j - 1])
+                    return false;
+        return true;
+    }
+};
+
+
+
+/*https://leetcode.com/problems/toeplitz-matrix/*/
+
+/*Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.*/
