7d9eef367f060f94e104da14687bc421c10ec0ea
HarshTiwari6864 <harshtiwari6864@gmail.com>
2025-12-01 17:16:27 +0530

    .

---
+++ compare_version_number.cpp
@@ -0,0 +1,29 @@
+_class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0, j = 0;
+
+        while (i < version1.size() || j < version2.size()) {
+            long x1 = 0, x2 = 0;
+
+            while (i < version1.size() && version1[i] != '.') {
+                x1 = x1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            while (j < version2.size() && version2[j] != '.') {
+                x2 = x2 * 10 + (version2[j] - '0');
+                j++;
+            }
+
+            if (x1 > x2) return 1;
+            if (x1 < x2) return -1;
+
+            // skip dots
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
+/*https://leetcode.com/problems/compare-version-numbers/description/?envType=daily-question&envId=2025-09-23*/
---
+++ meet_intersect_linked_list.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* lista = headA;
+        ListNode* listb = headB;
+
+        while (lista != listb) {
+            lista = (lista != nullptr) ? lista->next : headB;
+            listb = (listb != nullptr) ? listb->next : headA;
+        }
+
+        return lista;        
+    }
+};
+
+/*https://leetcode.com/problems/intersection-of-two-linked-lists/description/*/
